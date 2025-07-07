744f96a4d74efd91a73c4c44fa71c3fbb885fe2d
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-08 00:49:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 29245477
 API_HASH = "0abc83883262245c90ca337b7a0375c4"
-BOT_TOKEN = "7899"
-MONGO_URI = ""
-ADMINS = [7086472788]  # Replace with your Telegram user ID(s)
-START_PIC = "https://images6.alphacoders.com/132/1323207.jpeg"
-LINK_PIC = "https://images7.alphacoders.com/132/1323209.jpeg"
+BOT_TOKEN = "0"
+MONGO_URI = "mongodb+srv://Kafka:Au3OoWzCDYJKeuHU@cluster0.lz2m8iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ADMINS = [7654385403]  # Replace with your Telegram user ID(s)
+START_PIC = "https://litter.catbox.moe/bxry4e.jpg"
+LINK_PIC = "https://files.catbox.moe/gyklg0.jpg"
