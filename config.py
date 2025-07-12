9577391c5a5104bcb344a7b89dad90e54eee4861
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-12 11:19:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 29245477
 API_HASH = "0abc83883262245c90ca337b7a0375c4"
-BOT_TOKEN = ""
+BOT_TOKEN = "7845096754:AAGp_ydOCzsjrC89b3LOWHzuWG4gKTdK2uI"
 MONGO_URI = "mongodb+srv://Kafka:Au3OoWzCDYJKeuHU@cluster0.lz2m8iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ADMINS = [7654385403]  # Replace with your Telegram user ID(s)
 START_PIC = "https://litter.catbox.moe/bxry4e.jpg"
