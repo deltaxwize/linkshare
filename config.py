6bcca8690c82640c572155a7b38d6af7d3214d2e
deltaxwize <lustfantasyofc@gmail.com>
2025-09-26 17:30:49 +0530

    Link

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 29245477
-API_HASH = "0abc83883262245c90ca337b7a0375c4"
-BOT_TOKEN = ""
-MONGO_URI = ""
-ADMINS = [7654385403]  # Replace with your Telegram user ID(s)
+API_ID = 21375775
+API_HASH = "24a8567a7fa9209f79d4643b191c1ae4"
+BOT_TOKEN = "8352589481:AAGIEWnvzHYY4qDtAjOnGOM9nKNGtK2zQHs"
+MONGO_URI = "mongodb+srv://linkshare:linkshare@cluster0.c1c6uc2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ADMINS = [7668133494 8076259467]  # Replace with your Telegram user ID(s)
 START_PIC = "https://litter.catbox.moe/bxry4e.jpg"
 LINK_PIC = "https://files.catbox.moe/gyklg0.jpg"
