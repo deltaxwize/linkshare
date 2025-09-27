cf92d595c5da3e3096cce1482cc5b9cd498293e7
psysinner <psychosinner@proton.me>
2025-09-27 15:46:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 12345678
-API_HASH = "24c3a9d"
-BOT_TOKEN = "7899"
-MONGO_URI = ""
-ADMINS = [7086472788]  # Replace with your Telegram user ID(s)
-START_PIC = "https://images6.alphacoders.com/132/1323207.jpeg"
-LINK_PIC = "https://images7.alphacoders.com/132/1323209.jpeg"
+API_ID = 21375775
+API_HASH = "24a8567a7fa9209f79d4643b191c1ae4"
+BOT_TOKEN = "8352589481:AAGIEWnvzHYY4qDtAjOnGOM9nKNGtK2zQHs"
+MONGO_URI = "mongodb+srv://linkshare:linkshare@cluster0.c1c6uc2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ADMINS = [7668133494,8076259467]  # Replace with your Telegram user ID(s)
+START_PIC = "https://litter.catbox.moe/bxry4e.jpg"
+LINK_PIC = "https://files.catbox.moe/gyklg0.jpg"
